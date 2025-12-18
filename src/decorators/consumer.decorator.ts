faff7a1d283a50951fa2cc579e722de7e724dc2b
Vaibhav Bhalla <vaibhav.bhalla@sourcefuse.com>
2025-12-18 12:18:56 +0530

    docs(chore): docs update
    docs update
    
    BREAKING CHANGE:
    YES
    
    GH-99

--- src/decorators/consumer.decorator.ts
+++ src/decorators/consumer.decorator.ts
@@ -2,6 +2,13 @@ import {Constructor, injectable} from '@loopback/core';
 import {asConsumer} from '../keys';
 import {IConsumer, IStreamDefinition} from '../types';
 
+/**
+ * The `consumer` function in TypeScript is a generic function that returns an injectable consumer for
+ * a specific stream definition and message type.
+ * @returns The `consumer` function is returning a function that takes a target constructor of type
+ * `IConsumer<T, E>` and does some operation on it. The specific operation being performed is not clear
+ * from the provided code snippet.
+ */
 export function consumer<
   T extends IStreamDefinition,
   E extends keyof T['messages'],
